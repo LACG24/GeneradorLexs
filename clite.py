40e78ab14d49bd12fa9f3e05ede0b1553f8f78c7
LACG24 <LACG24@outlook.com>
2025-03-29 23:30:49 -0600

    Primer prueba FLOAT

--- clite.py
+++ clite.py
@@ -4,13 +4,18 @@ tokens = ['INT', 'FLOAT']
 
 t_ignore = ' \t'
 
+def t_FLOAT(t):
+    r'([0-9]+\.[0-9]*|\.[0-9]+)'
+    t.value = float(t.value)
+    return t
+
 # Numeros con guion bajo 'si', pero no dos consecutivos
 def t_INT(t):
     r'[0-9]+(_[0-9]+)*'
-    t.value = int(t.value.replace('_', '')) # Reemplazar guiones bajos
+    t.value = int(t.value.replace('_', ''))
     return t
 
-#Para que no salga el warning
+# Funcion para manejar errores
 def t_error(t):
     raise lex.LexError(f"Illegal character '{t.value[0]}'", t.value[0])
 
