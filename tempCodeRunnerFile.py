ba4f238fd2005f379ee99367d1910a6b9c659279
Raul-Jimenez2913 <jimenezhernandezchristianraul@gmail.com>
2025-03-29 20:29:01 -0600

    Limpieza de archivos erroneos

--- tempCodeRunnerFile.py
+++
@@ -1,6 +0,0 @@
-    def test_wrong_integers(self):
-        self.lexer.input('1__934')
-        token = self.lexer.token()
-        self.assertEqual(token.type, 'INT')
-        self.assertNotEqual(token.value, 1934)
-        self.assertRaises(lex.LexError, self.lexer.token)
